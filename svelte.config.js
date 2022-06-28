552ecf8077ced934fa10e096cb96fe74d280adaa
Unchaptered <workstation19961002@gmail.com>
2022-06-28 17:23:04 +0900

    Apply sass autoprefixer, support sass in front

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+
+import sveltePreprocess from 'svelte-preprocess';
+import svelteAutoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: sveltePreprocess({
+		postcss: {
+			plugins: [
+				svelteAutoprefixer(),
+			],
+			
+		},
+	}),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
 	}
 };
 
