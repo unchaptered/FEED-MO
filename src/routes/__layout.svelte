a2411e0c227aee8606fbf3c6c4bb7430307123a4
Unchaptered <workstation19961002@gmail.com>
2022-06-28 17:25:32 +0900

    Apply components, include navigation and footer

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script lang="js">
+    
+    import Nav from '../components/navigation.svelte';
+    import Footer from '../components/footer.svelte';
+
+</script>
+
+<Nav />
+
+<main>
+    <slot />
+</main>
+
+<Footer />
+
+<style global>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+</style>
