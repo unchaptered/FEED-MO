5c9f3762aae9619cf39165296296985d66614bef
Unchaptered <workstation19961002@gmail.com>
2022-06-29 16:51:04 +0900

    Move link.ts

---
+++ src/constants/link.ts
@@ -0,0 +1,16 @@
+export const repositories = {
+    frontend: 'https://github.com/unchaptered/FEED-MO',
+    backend: 'https://github.com/unchaptered/feedback-your-project'
+};
+
+export const routes = {
+    forntend: {
+        home: '/',
+        join: '/join',
+        login: '/login'
+    },
+    backend: {
+        join: '/join',
+        login: '/login'
+    }
+}
--- src/constants/sizes.ts
+++
@@ -1,5 +0,0 @@
-export const sizes = {
-    nav: {
-        height: 300
-    }
-}
